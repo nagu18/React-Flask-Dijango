22a38842553eddd90b3caacaba1a7ca8137a2757
Nagu <kondanagendher8@gmail.com>
2025-07-15 14:53:35 +0530

    update

--- Django/e_book/e_book/settings.py
+++ Django/e_book/e_book/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'data',
 ]
 
 MIDDLEWARE = [
@@ -115,7 +116,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+LOGIN_URL = 'login'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
