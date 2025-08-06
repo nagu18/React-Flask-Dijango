90e887e36d036c9f2a5f4014f55712c43956cf92
Nagu <kondanagendher8@gmail.com>
2025-08-06 11:03:40 +0530

    update

--- Django/code_chat/code_chat/settings.py
+++ Django/code_chat/code_chat/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#cp$p_y@1u#1*+*&%koobn8n3u+s9g^nk%5&g6e+4^=l4xb8-j'
-
+SECRET_KEY = os.getenv("Django_Key")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -72,11 +73,14 @@ WSGI_APPLICATION = 'code_chat.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT',),
     }
 }
 
